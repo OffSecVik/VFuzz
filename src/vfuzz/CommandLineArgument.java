80d898605668c284b9b0b94f1041612f548b06c1
Niclassslua <niclas.frey@gmx.de>
2024-03-16 01:07:31 +0100

    Small changes to reduce warnings

--- src/vfuzz/CommandLineArgument.java
+++ src/vfuzz/CommandLineArgument.java
@@ -4,19 +4,19 @@ import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
 public class CommandLineArgument {
-    private String name;
-    private String alias;
-    private String configName; // Hinzugefügt, um den Konfigurationsnamen zu speichern
-    private BiConsumer<ConfigManager, String> action;
-    private Predicate<String> validator;
-    private String description;
-    private boolean isOptional;
-    private String defaultValue;
+    private final String name;
+    private final String alias;
+    private final String configName;
+    private final BiConsumer<ConfigManager, String> action;
+    private final Predicate<String> validator;
+    private final String description;
+    private final boolean isOptional;
+    private final String defaultValue;
 
     public CommandLineArgument(String name, String alias, String configName, BiConsumer<ConfigManager, String> action, Predicate<String> validator, String description, boolean isOptional, String defaultValue) {
         this.name = name;
         this.alias = alias;
-        this.configName = configName; // Speichere den Konfigurationsnamen
+        this.configName = configName;
         this.action = action;
         this.validator = validator;
         this.description = description;
@@ -32,7 +32,7 @@ public class CommandLineArgument {
         return alias;
     }
 
-    public String getDescription() {
+    public String getDescription() { //TODO: --help
         return description;
     }
 
@@ -53,7 +53,7 @@ public class CommandLineArgument {
             action.accept(configManager, argValue);
         } else {
             System.out.println("Validation failed for argument: " + name + " with value: " + argValue);
-            // Optional: Hier könntest du entscheiden, ob bei Fehlschlag der Validierung der Standardwert verwendet werden soll
+            //TODO: What happens after failed Validation?
         }
     }
 
